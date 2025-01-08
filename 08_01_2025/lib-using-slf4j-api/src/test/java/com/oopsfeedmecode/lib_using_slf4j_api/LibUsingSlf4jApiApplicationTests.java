7867fdef8f589d16955a33e4bbdcea4f168d87eb
arunveersingh <arunveersingh9@gmail.com>
2025-01-08 23:28:30 +0530

    Log4J Demo projects structure

---
+++ 08_01_2025/lib-using-slf4j-api/src/test/java/com/oopsfeedmecode/lib_using_slf4j_api/LibUsingSlf4jApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oopsfeedmecode.lib_using_slf4j_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibUsingSlf4jApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
