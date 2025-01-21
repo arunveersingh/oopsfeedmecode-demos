15ad72e32729245cf25df6c6a836d8ac52396601
arunveersingh <arunveersingh9@gmail.com>
2025-01-21 23:32:20 +0530

    Demo spring-jcl is mandatory for spring framework to start and print logs

--- 08_01_2025/app/src/main/java/com/oopsfeedmecode/app/AppApplication.java
+++ 08_01_2025/app/src/main/java/com/oopsfeedmecode/app/AppApplication.java
@@ -1,18 +1,18 @@
 package com.oopsfeedmecode.app;
 
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AppApplication {
-	private static final Logger logger = LogManager.getLogger(AppApplication.class.getName());
+	//private static final Logger logger = LogManager.getLogger(AppApplication.class.getName());
 
 	public static void main(String[] args) {
-		logger.debug("Starting app");
+		//logger.debug("Starting app");
 		SpringApplication.run(AppApplication.class, args);
-		logger.debug("Started app");
+		//logger.debug("Started app");
 	}
 }
